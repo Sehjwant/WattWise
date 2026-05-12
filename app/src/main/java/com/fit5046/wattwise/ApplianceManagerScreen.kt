4908118e3f24b6d539c615a59dafdd32644175e6
Sehjwant Singh <sehj0001@student.monash.edu>
2026-05-12 17:48:22 +1000

    feat(appliances): add CategoryStyle data class and getCategoryStyle helper

--- app/src/main/java/com/fit5046/wattwise/ApplianceManagerScreen.kt
+++ app/src/main/java/com/fit5046/wattwise/ApplianceManagerScreen.kt
@@ -17,4 +17,18 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+data class CategoryStyle(
+    val color: Color,
+    val backgroundColor: Color,
+    val icon: ImageVector
+)
+
+fun getCategoryStyle(category: String): CategoryStyle = when (category) {
+    "Cooling"  -> CategoryStyle(Color(0xFF1565C0), Color(0xFFE3F2FD), Icons.Default.AcUnit)
+    "Heating"  -> CategoryStyle(Color(0xFFB71C1C), Color(0xFFFFEBEE), Icons.Default.Fireplace)
+    "Laundry"  -> CategoryStyle(Color(0xFF6A1B9A), Color(0xFFF3E5F5), Icons.Default.LocalLaundryService)
+    "Kitchen"  -> CategoryStyle(Color(0xFFE65100), Color(0xFFFFF3E0), Icons.Default.Kitchen)
+    "Lighting" -> CategoryStyle(Color(0xFFF9A825), Color(0xFFFFFDE7), Icons.Default.LightMode)
+    else       -> CategoryStyle(Color(0xFF455A64), Color(0xFFECEFF1), Icons.Default.Power)
+}
 
